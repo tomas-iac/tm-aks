bc4045e0c2a41969625b7eb63610a65f41fe8938
Tomas Kubica <tkubica12@gmail.com>
2022-04-07 07:55:52 +0200

    increase max pods

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -3,6 +3,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = var.location
   resource_group_name = var.resourceGroupName
   dns_prefix          = "aks-${var.name}"
+  max_pods            = 70
 
   default_node_pool {
     name           = "default"
